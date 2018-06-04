2ee7fb8806e63045b2c5c9ae757ea433138f9ae0
kacgaw <kacper.gawronski.public@gmail.com>
2018-06-04 19:41:02 +0200

    Before macroization

--- test.py
+++ test.py
@@ -1,23 +1,16 @@
 import numpy as np
 from gauss import *
 from time import time
-<<<<<<< HEAD
 '''
 h=1500
 a=((np.random.random(h*h))).reshape(h,h)
 b=(np.random.random(h))
-=======
-h=2000
-a=((np.random.random(h*h)*10)//1).reshape(h,h)
-b=(np.random.random(h)*10)//1
->>>>>>> 5414dcd7ee34026e83a26dc57bd0ad241558fd6e
 start=time()
 tmp=np.linalg.solve(a,b)
 print(time()-start)
 start=time()
 tmp=eliminate(a,b)
 print(time()-start)
-<<<<<<< HEAD
 '''
 h=3
 a1=np.random.random(h*h)
@@ -29,5 +22,3 @@ b=np.array(list(map(lambda x:complex(*x),zip(b1,b2)))).reshape(h)
 
 print(np.linalg.solve(a,b))
 print(eliminate(a,b))
-=======
->>>>>>> 5414dcd7ee34026e83a26dc57bd0ad241558fd6e
